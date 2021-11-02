c96e61746bb7b49896bc10d8445a963eb6637a2c
Abhishek Valsan <abhishekvalsan9999@gmail.com>
2021-11-03 01:22:07 +0530

    code refactor ind gov scrap

--- scrap_ind_gov.py
+++ scrap_ind_gov.py
@@ -9,27 +9,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from contextlib import closing
 
 def scrap_page(url):
-	print "\n\n\nScrapping :: ",url,"\n\n\n"
+	print("Scrapping ",url)
 	options = Options()
 	options.add_argument("--headless")
 	url_list=[]
 	with closing(Firefox(firefox_options=options)) as driver:
 		driver.get(url)
-		WebDriverWait(driver, timeout=10).until(lambda x: x.find_elements_by_tag_name('div'))
-		pages_remaining 	= True
+		WebDriverWait(driver, timeout=10).until(
+								lambda x: x.find_elements_by_tag_name('div'))
+		pages_remaining = True
 		while pages_remaining:
-			page_source	= driver.page_source
-			soup		= BeautifulSoup(page_source,'html.parser')
-			links=soup.findAll("a")
+			page_source = driver.page_source
+			soup = BeautifulSoup(page_source,'html.parser')
+			links = soup.findAll("a")
 			for link in links:
 				try:
-					if link['title'][:4]=="http":
-						print link['title'].split(' -')[0]
+					if link['title'].startswith("http"):
+						print(link['title'].split(' -')[0])
 						url_list.append(link['title'].split(' -')[0])
 				except:
 					pass
 			try:
-				next_link = driver.find_element_by_xpath("//div[@class='pagination']//a[@title='Next']")
+				next_link = driver.find_element_by_xpath(
+								"//div[@class='pagination']//a[@title='Next']")
 				next_link.click()
 				time.sleep(3)
 			except NoSuchElementException:
@@ -54,45 +56,10 @@ def create_url_list():
 		'http://goidirectory.nic.in/judiciary_subcategory.php?ct=J011',\
 	]
 	#STATE
-	state_codes=[
-		'AN',\
-		'AP',\
-		'AR',\
-		'AS',\
-		'BR',\
-		'CH',\
-		'CG',\
-		'DN',\
-		'DD',\
-		'DL',\
-		'GA',\
-		'GJ',\
-		'HR',\
-		'HP',\
-		'JK',\
-		'JH',\
-		'KA',\
-		'KL',\
-		'LD',\
-		'MP',\
-		'MN',\
-		'MH',\
-		'ML',\
-		'MZ',\
-		'NL',\
-		'OR',\
-		'PY',\
-		'PB',\
-		'RJ',\
-		'SK',\
-		'TN',\
-		'TG',\
-		'TR',\
-		'UK',\
-		'UP',\
-		'WB',\
-	]
-	categories=[
+	state_codes = ['AN','AP','AR','AS','BR','CH','CG','DN','DD','DL','GA',\
+		'GJ','HR','HP','JK','JH','KA','KL','LD','MP','MN','MH','ML','MZ',\
+		'NL','OR','PY','PB','RJ','SK','TN','TG','TR','UK','UP','WB']
+	categories = [
 		'http://goidirectory.nic.in/state_category.php?ou=####&ct=E001',\
 		'http://goidirectory.nic.in/state_depts.php?ou=####&ct=E003',\
 		'http://goidirectory.nic.in/state_orgn_categories.php?ou=####&ct=E005',\
@@ -103,14 +70,17 @@ def create_url_list():
 	]
 	for state_code in state_codes:
 		for category in categories:
-			urls.append(c.split("####")[0]+state_code+category.split("####")[-1])
+			urls.append( c.split("####")[0] + state_code + \
+							category.split("####")[-1])
 
 	#MINISTRY part in the UNION
 	ministry_num=range(52)+[723,56,1147,248,4331]
 	ministry_group=[1,2,3,7,17,21]
 	for i in ministry_num:
 		for j in ministry_group:
-			urls.append('http://goidirectory.nic.in/ministries_categories.php?ct='+str(i)+'#group'+str(j))
+			urls.append(
+				'http://goidirectory.nic.in/ministries_categories.php?ct='+\
+				str(i)+'#group'+str(j))
 	print("Created mailing List.")
 	return urls
 
